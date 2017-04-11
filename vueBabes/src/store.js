4fa92a483780cee720f8352e482bef17bcaa5ee5
ikitty <xkeitty@163.com>
2017-04-12 00:48:38 +0800

    add store

--- vueBabes/src/store.js
+++ vueBabes/src/store.js
@@ -1,7 +1,24 @@
+import wilddog from 'wilddog'
+
 var STORAGE_KEY = 'vue-babes';
+
+var config = { syncURL: "https://bkmk.wilddogio.com/"};
+wilddog.initializeApp(config);
+const Ref = wilddog.sync().ref();
+
 
 export default {
     fetch: function () {
+        Ref.child('todo').on("value", function (data, error) {
+            var ret = []
+            if (error == null) {
+                var D = data.val()
+                ret = Object.keys(D).map( v=>{
+                    D[v]['id'] = v
+                    return  D[v] ;
+                })
+            }
+        });
         return JSON.parse(localStorage.getItem(STORAGE_KEY) || '[]');
     },
     save: function (todos) {
